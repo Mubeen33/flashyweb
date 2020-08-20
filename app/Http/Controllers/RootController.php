74ff89b6fb5cb168d7c0291884e1a5587aef4d2d
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2020-08-20 19:55:45 +0600

    Developer Rijan

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -4,11 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use Carbon\Carbon;
 
 class RootController extends Controller
 {
     public function index(){
-    	$sliders = Slider::orderBy('order_no', 'ASC')->get();
+    	$current = Carbon::now();
+    	$today =$current->format('Y-m-d') ;
+    	$sliders = Slider::whereDate('start_time', '<=', $today)
+    					->whereDate('end_time', '>=', $today)
+    					->orderBy('order_no', 'ASC')
+    					->get();
     	return view('index', compact('sliders'));
     }
 }
