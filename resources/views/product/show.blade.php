8a5a3e9d14671fd371a5f2b9d8323ebd76b23b07
RI Rijan <38884716+DeveloperRijan@users.noreply.github.com>
2020-09-20 17:39:46 +0600

    Developer Rijan

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -475,8 +475,8 @@
                     <h3>Related products</h3>
                 </div>
                 <div class="ps-section__content">
-                    <div class="ps-carousel--nav owl-slider" data-owl-auto="true" data-owl-loop="true" data-owl-speed="10000" data-owl-gap="30" data-owl-nav="true" data-owl-dots="true" data-owl-item="6" data-owl-item-xs="2" data-owl-item-sm="2" data-owl-item-md="3" data-owl-item-lg="4" data-owl-item-xl="5" data-owl-duration="1000" data-owl-mousedrag="on">
-                        @include('product.partials.related-products')     
+                    <div class="ps-carousel--nav owl-slider" data-owl-auto="false" data-owl-loop="false" data-owl-speed="10000" data-owl-gap="30" data-owl-nav="true" data-owl-dots="true" data-owl-item="7" data-owl-item-xs="2" data-owl-item-sm="3" data-owl-item-md="4" data-owl-item-lg="5" data-owl-item-xl="6" data-owl-duration="1000" data-owl-mousedrag="on">
+                        @include('product.partials.related-products', ['products'=>$related_products])
                     </div>
                 </div>
             </div>
