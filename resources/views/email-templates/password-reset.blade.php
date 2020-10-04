7157c8b79fa9a105edbc5ee2b5b4eb09df5f9be4
Mubeen33 <mubeenbaig33@gmail.com>
2020-10-05 01:21:57 +0500

    Mubeen

--- resources/views/email-templates/password-reset.blade.php
+++ resources/views/email-templates/password-reset.blade.php
@@ -1,81 +1,327 @@
-<!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>Password Reset Email</title>
-	<style type="text/css">
-		.body{
-			width: 100%;
-			margin: 0 auto;
-			background: #ddd;
-			padding:40px 20px
-		}
-		
-		.body .header h3{
-			text-align:center;
-			font-size:35px;
-		}
-		
-		.body .content p{
-			font-size:18px;
-		}
-		.body .content .link-btn{
-			margin-top: 30px;
-		}
-		
-		.body .content .link-btn a{
-			text-decoration: none;
-			text-transform: uppercase;
-			background: #7952b3;
-			color: #fff;
-			padding: 10px 20px;
-			border-radius: 2px;
-		}
-		
-		
-		.body .footer{
-			margin-top:100px
-		}
-		.body .footer p{
-			text-align:center;
-		}
-		
-		.body .social_links{
-			display: flex;
-			justify-content: center;
-		}
-		.body .social_links a{
-			text-decoration: none;
-			background: #878787;
-			color: #fff;
-			padding: 5px 10px;
-			margin-right: 2px;
-			border-radius: 3px;
-		}
-	</style>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+<title>Wohoo</title>
+<style type="text/css">
+div, p, a, li, td {
+  -webkit-text-size-adjust:none;
+}
+.ReadMsgBody {
+  width: 100%;
+  background-color: #cecece;
+}
+.ExternalClass {
+  width: 100%;
+  background-color: #cecece;
+}
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #cecece;
+  margin:0;
+  padding:0;
+  -webkit-font-smoothing: antialiased;
+}
+html {
+  width: 100%;
+}
+img{
+  border:none;
+  }
+table td[class=show]{
+  display:none !important;
+  }
+@media only screen and (max-width: 640px) {
+body {
+  width:auto!important;
+}
+table[class=full] {
+ width:100%!important;
+}
+table[class=devicewidth] {
+ width:100% !important;
+ padding-left:20px !important;
+ padding-right: 20px!important;
+}
+table[class=inner] {
+ width:100%!important;
+ text-align: center!important;
+ clear: both;
+}
+table[class=inner-centerd] {
+ width:78%!important;
+ text-align: center!important;
+ clear: both;
+ float:none !important;
+ margin:0 auto !important;
+}
+table td[class=hide], .hide {
+ display:none !important;
+}
+table td[class=show], .show{
+  display:block !important;
+  }
+img[class=responsiveimg]{
+ width:100% !important;
+ height:atuo !important;
+ display:block !important;
+}
+table[class=btnalign]{
+  float:left !important;
+  }
+table[class=btnaligncenter]{
+  float:none !important;
+  margin:0 auto !important;
+  }
+table td[class=textalignleft]{
+  text-align:left !important;
+  padding:0 !important;
+  }
+table td[class=textaligcenter]{
+  text-align:center !important;
+  }
+table td[class=heightsmalldevices]{
+  height:45px !important;
+  }
+table td[class=heightSDBottom]{
+  height:28px !important;
+  }
+table[class=adjustblock]{
+  width:87% !important;
+  }
+table[class=resizeblock]{
+  width:92% !important;
+  }
+table td[class=smallfont]{
+  font-size:8px !important;
+  }
+  }
+@media only screen and (max-width: 520px) {
+table td[class=heading]{
+  font-size:24px !important;
+  }
+table td[class=heading01]{
+  font-size:18px !important;
+  }
+table td[class=heading02]{
+  font-size:27px !important;
+  }
+table td[class=text01]{
+  font-size:22px !important;
+  }
+table[class="full mhide"], table tr[class=mhide]{
+  display:none !important;
+  }
+  }
+@media only screen and (max-width: 480px) {
+table {
+ border-collapse: collapse;
+}
+table[id=colaps-inhiret01],
+table[id=colaps-inhiret02],
+table[id=colaps-inhiret03],
+table[id=colaps-inhiret04],
+table[id=colaps-inhiret05],
+table[id=colaps-inhiret06],
+table[id=colaps-inhiret07],
+table[id=colaps-inhiret08],
+table[id=colaps-inhiret09]{
+  border-collapse:inherit !important;
+  }
+}
+@media only screen and (max-width: 320px) {
+  }
+</style>
 </head>
 <body>
-	<section class="body">
-		<div class="header">
-			<h3>Flashybuy</h3>
-		</div>
+<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" class="full">
+  <tr>
+    <td align="center"><table width="600" border="0" cellspacing="0" cellpadding="0" align="center" class="devicewidth">
+        <tr>
+          <td><table width="100%" bgcolor="#ffffff" border="0" cellspacing="0" cellpadding="0" align="center" class="full" style="background-color:#ffffff; border-radius:5px 5px 0 0;">
+              <tr>
+                <td><table width="265" align="left" border="0" cellspacing="0" cellpadding="0" class="inner" style="text-align:center;">
+                <tr>
+                      <td width="28" height="52">&nbsp;</td>
+                      <td>&nbsp;</td>
+                    </tr>
+                    <tr>
+                    <td width="28">&nbsp;</td>
+                      <td align="center" valign="middle"><a href="#"><img src="{{ asset('email/images/logo.png')}}" width="140" height="35" alt="Wohoo"></a></td>
+                    </tr>
+                    <tr>
+                    </tr>
+                  </table>
+                  <table width="255" align="right" border="0" cellspacing="0" cellpadding="0" class="inner" style="text-align:center;">
+                <tr>
+                      <td class="hide" height="22">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td height="10">&nbsp;</td>
+                    </tr>
+                    <tr>
+                      <td align="center" valign="middle">
+                        <table width="191" align="center" border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; text-align:center;">
+  <tr>
+  <td align="center" bgcolor="#f27b69" height="57" style="border-radius:28px;"><a href="#" style="font-family:'Montserrat', Helvetica, Arial, sans-serif; font-weight:700; font-size:16px; line-height:57px; color:#ffffff; text-decoration:none; text-transform:uppercase; display:block; overflow:hidden; ">GO TO FlashyBuy</a></td>
+  </tr>
+</table>
 
-		<div class="content">
-			<p><b>Hi, {{ $firstName }} {{ $lastName }}</b></p>
-			<p>Your password reset link. Please click the button to reset password.</p>
-			
-			<div class='link-btn'>
-				<a href='{{ $siteURL."/reset/passoword/".$token."/".$email }}'>Reset</a>
-			</div>
-		</div>
+                      </td>
+                    </tr>
+                     <tr>
+                      <td height="40">&nbsp;</td>
+                    </tr>
+                  </table>
+                  </td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+<!-- ----------------- Header End Here ------------------------- -->
+<table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" class="full">
+  <tr>
+  <td align="center">
+  	<table width="600" border="0" cellspacing="0" cellpadding="0" align="center" class="devicewidth">
+  <tr>
+    <td>
+    	<table width="100%" bgcolor="#ffffff"  border="0" cellspacing="0" cellpadding="0" align="center" class="full" style="text-align:center; border-bottom:1px solid #e5e5e5;">
+   <tr>
+  	<td class="heightsmalldevices" height="40">&nbsp;</td>
+  </tr>
+  <tr>
+  	<td style="font:700 30px 'Montserrat', Helvetica, Arial, sans-serif; color:#f27b69; text-transform:uppercase;">change your password</td>
+  </tr>
+   <tr>
+  	<td class="heightSDBottom" height="49">&nbsp;</td>
+  </tr>
+  <tr>
+  	<td style="font:bold 18px Arial, Helvetica, sans-serif; color:#404040; text-transform:uppercase;">Hi {{ $firstName }} {{ $lastName }}</td>
+  </tr>
+   <tr>
+  	<td height="21">&nbsp;</td>
+  </tr>
+  <tr>
+  	<td style="font:18px Arial, Helvetica, sans-serif; color:#404040;">We got a requrest to reset your password.</td>
+  </tr>
+   <tr>
+  	<td height="32">&nbsp;</td>
+  </tr>
+  <tr>
+                      <td align="center">
+                      	<table width="250" align="center" border="0" cellspacing="0" cellpadding="0" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; text-align:center;">
+  <tr>
+  <td align="center" bgcolor="#af5372" style="border-radius:28px;" height="61"><a href="'{{ $siteURL."/reset/passoword/".$token."/".$email }}'" style="font:700 16px/61px 'Montserrat', Helvetica, Arial, sans-serif; color:#ffffff; text-decoration:none; display:block; overflow:hidden; outline:none;">RESET PASSWORD</a></td>
+  </tr>
+</table>
 
-		<div class="footer">
-			<p>Flashybuy - <?php echo date('Y');?> all rights reserved.</p>
-			<div class="social_links">
-				<a href="#">Facebook</a>
-				<a href="#">Twitter</a>
-				<a href="#">LinkedIn</a>
-			</div>
-		</div>
-	</section>
+                      </td>
+                    </tr>
+                    <tr>
+  	<td class="heightsmalldevices" height="60">&nbsp;</td>
+  </tr>
+  </table>
+    </td></tr>
+    </table>
+  </td></tr></table>
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" class="full">
+  <tr>
+  <td align="center">
+    <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" class="devicewidth">
+  <tr>
+    <td>
+      <table width="100%" bgcolor="#ffffff"  border="0" cellspacing="0" cellpadding="0" align="center" class="full" style="text-align:center; border-bottom:1px solid #e5e5e5;">
+   <tr>
+    <td height="50">&nbsp;</td>
+  </tr>
+  <tr>
+    <td style="font:16px Arial, Helvetica, sans-serif; color:#404040; padding:0 10px;">If you ignore this message, your password won't be changed.</td>
+  </tr>
+   <tr>
+    <td height="28">&nbsp;</td>
+  </tr>
+ <tr>
+    <td style="font:16px Arial, Helvetica, sans-serif; color:#404040; padding:0 10px;">If you didn't request a password reset, <a href="#" style="color:#f27b69; text-decoration:none;">let us know</a></td>
+  </tr>
+  <tr>
+    <td height="32">&nbsp;</td>
+  </tr>
+   <tr>
+    <td style="font:12px Arial, Helvetica, sans-serif; color:#b1b1b1;">if this is not your account you can <br class="show" style="display:none;"> <a href="#" style="color:#b1b1b1; font-weight:bold;">remove your email from it.</a></td>
+  </tr>
+  <tr>
+  <tr>
+    <td class="heightsmalldevices" height="60">&nbsp;</td>
+  </tr>
+  </table>
+    </td></tr>
+    </table>
+  </td></tr></table
+  <!-- footer -->
+  <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" class="full">
+  <tr>
+  <td align="center">
+    <table width="600" border="0" cellspacing="0" cellpadding="0" align="center" class="devicewidth">
+  <tr>
+    <td>
+      <table width="100%" bgcolor="#b5637e" border="0" cellspacing="0" cellpadding="0" align="center" class="full" style="border-radius:0 0 5px 5px;">
+        <tr><td height="18">&nbsp;</td></tr>
+  <tr>
+  <td>
+  <table width="340" border="0" cellspacing="0" cellpadding="0" align="right" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; text-align:center;" class="inner">
+  <tr>
+  <td width="21">&nbsp;</td>
+    <td>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+    <tr><td>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center" class="full">
+  <tr>
+    <td align="center" style="font:11px Helvetica,  Arial, sans-serif; color:#383838;"><a style="color:#ffffff; text-decoration:none;" href="#"> View in browser</a> </td>
+    <td style="color:#ffffff;"> | </td>
+    <td align="center" style="font:11px Helvetica,  Arial, sans-serif; color:#383838;"><a style="color:#ffffff; text-decoration:none;" href="#"> Unsubscribe</a> </td>
+    <td style="color:#ffffff;"> | </td>
+    <td align="center" style="font:11px Helvetica,  Arial, sans-serif; color:#383838;"><a style="color:#ffffff; text-decoration:none;" href="#"> Send to a friend</a> </td>
+    <td class="hide" width="40" align="right">&nbsp;</td>
+  </tr>
+  <tr><td height="18">&nbsp;</td></tr>
+</table>
+
+    </td></tr>
+</table>
+
+    </td>
+  </tr>
+</table>
+
+    <table width="230" border="0" cellspacing="0" cellpadding="0" align="left" style="border-collapse:collapse; mso-table-lspace:0pt; mso-table-rspace:0pt; text-align:center;" class="inner">
+  <tr>
+    <td width="21">&nbsp;</td>
+    <td>
+      <table width="100%" border="0" cellspacing="0" cellpadding="0" align="center">
+    <tr><td align="center" style="font:11px Helvetica,  Arial, sans-serif; color:#ffffff;">&copy; 2014, All rights reserved
+</td></tr>
+<tr><td height="18">&nbsp;</td></tr>
+</table>
+
+    </td>
+    <td width="21">&nbsp;</td>
+  </tr>
+</table>
+
+  </td></tr>
+  </table>
+    </td></tr>
+    <tr class="mhide">
+    <td height="100">&nbsp;</td>
+    </tr>
+    </table>
+  </td></tr></table>
+  
 </body>
 </html>
