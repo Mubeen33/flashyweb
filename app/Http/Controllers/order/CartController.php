937b100c036037313d4bd8371dfc2e62dfe0e2a8
Mubeen33 <mubeenbaig33@gmail.com>
2020-10-05 11:52:09 +0500

    Mubeeen

--- app/Http/Controllers/order/CartController.php
+++ app/Http/Controllers/order/CartController.php
@@ -196,6 +196,16 @@ if(isset($_POST['action']) && $_POST['action'] == "empty"){
 			}
 
 			session()->forget('cart');
+			
+			$order   = Order::where('order_id',$order_id)->get();
+            if ($order) {
+
+                $subject = 'Your order# ("'.$orderID.') at FlashyBuy';
+                $email = Auth::guard('customer')->user()->email;
+
+                Mail::to($email)->send(new OrderMail(
+                    $order, $subject
+                 ));
     		return redirect()->route('customer.orders.index')->with('success', 'Order Saved Successfully');
 
 		}else{
---
+++ app/Mail/OrderMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class OrderMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    public $subject;
+    public $data;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($subject,$order)
+    {
+       $this->subject = $subject;
+       $this->data   = $order;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $emailSendNow = $this->subject($this->subject)->view('email-templates.order')
+        ->with([
+            
+            'data' => $this->data,
+        ]);
+        return $emailSendNow;
+    }
+}
